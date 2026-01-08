276780e35da2f43274a455a9cdcabbd6df20a699
Husnul <husnulf936@gmail.com>
2026-01-08 19:41:16 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Husnul"
+	fmt.Println(name)
+
+	name = "Fikri"
+	fmt.Println(name)
+
+	// 
+
+		var (
+		firstName = "Husnul"
+		lastName = "Fikri"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
+
