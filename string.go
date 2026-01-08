fdc9f5c75a7ffd602eae9605e2487c899c3db115
Husnul <husnulf936@gmail.com>
2026-01-08 19:30:50 +0700

    type data string

---
+++ string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Husnul"))
+	fmt.Println("Husnul Fikri"[0])
+	fmt.Println("Husnul Fikri"[1])
+
+	fmt.Println(len("Husnul"))
+	fmt.Println("Husnul Fikri"[0])
+	fmt.Println("Husnul Fikri"[1])
+}
+
