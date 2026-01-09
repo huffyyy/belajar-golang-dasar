51bdf63d801671e2a5859986c42d08d9120e2a55
Husnul <husnulf936@gmail.com>
2026-01-09 08:19:27 +0700

    comparison Operators

---
+++ comparison.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 = "Husnul"
+	var name2 = "Husnul"
+
+	var number1 = 1
+	var number2 = 2
+
+	var resultName bool = name1 == name2
+	var resultNumber bool = number2 < number1 
+
+	fmt.Println(resultName)
+	fmt.Println(resultNumber)
+}
